dce2d8946017c54b270fb55184ade96a27c62e39
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-09-08 12:13:43 +0200

    dependies update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
-axios.defaults.headers.common['x-api-key'] = 'твой ключ';
+axios.defaults.headers.common['x-api-key'] =
+  'live_xSM8F63uiCkUaigfVuS3xKGQfLylnItXSVq0tJtMbPf085967VBhZ1Nlohh8af31';
